69b1adbeca8a4d83d8c608e60a63197f867bf865
Rado <radoslav.danev@gmail.com>
2022-07-07 20:01:16 -0400

    adding setup to module

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rust_py1",
+    version="0.0.1",
+    author="Rado",
+    author_email="itsmebg@gmail.com",
+    description="Calculates a Fibonacci number",
+    long_description="A basic library that calculates Fibonacci numbers",
+    long_description_content_type="text/markdown",
+    url="",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    tests_require=['pytest'],
+)
